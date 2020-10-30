979aaed536fa3c12d637bff0dcdbffb56b0aed28
Jeff <jeffrezazada@gmail.com>
2020-10-30 19:56:47 -0400

    Outliers + To Do for saturday october 31st

--- Data_exploration.py
+++ Data_exploration.py
@@ -1,19 +1,20 @@
-#data exploration
+# data exploration
 # Import libraries
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pandas as pd
+
 pd.set_option('display.max_columns', None)
 data = pd.read_csv("cardio_train_updated.csv")
 
-print(data)
+#print(data)
+#print(data.dtypes)
+#print(data.describe())
 
-print(data.dtypes)
-
-print(data.describe())
-
-data['counts'] = 1
+# Counts
+"""data['counts'] = 1
 print(data[['counts', 'gluc']].groupby(['gluc']).agg('count'))
 data['counts'] = 1
 print(data[['counts', 'weight']].groupby(['weight']).agg('count'))
@@ -33,69 +34,133 @@ data['counts'] = 1
 print(data[['counts', 'ap_hi']].groupby(['ap_hi']).agg('count'))
 data['counts'] = 1
 print(data[['counts', 'cholesterol']].groupby(['cholesterol']).agg('count'))
+"""
+"""
+PLOTS
+"""
 
-#boxplot for height
-fig = plt.figure(figsize=(10, 10)) # Define plot area
-ax = fig.gca() # Define axis
+"""# boxplot for height
+fig = plt.figure(figsize=(10, 10))  # Define plot area
+ax = fig.gca()  # Define axis
 plt.boxplot(data.loc[:, 'height'])
-ax.set_title('Box plot of height (cm)') # Give the plot a main title
+ax.set_title('Box plot of height (cm)')  # Give the plot a main title
 ax.set_ylim(0.0, 300)
 plt.show()
 
-
-#boxplot for weight
-fig = plt.figure(figsize=(10, 10)) # Define plot area
-ax = fig.gca() # Define axis
+# boxplot for weight
+fig = plt.figure(figsize=(10, 10))  # Define plot area
+ax = fig.gca()  # Define axis
 plt.boxplot(data.loc[:, 'weight'])
-ax.set_title('Box plot of weight (kg)') # Give the plot a main title
+ax.set_title('Box plot of weight (kg)')  # Give the plot a main title
 ax.set_ylim(0.0, 300)
 plt.show()
 
-#boxplot for app_hi
-fig = plt.figure(figsize=(10, 10)) # Define plot area
-ax = fig.gca() # Define axis
+# boxplot for app_hi
+fig = plt.figure(figsize=(10, 10))  # Define plot area
+ax = fig.gca()  # Define axis
 plt.boxplot(data.loc[:, 'ap_hi'])
-ax.set_title('Box plot of systolic blood pressure (mmHg)') # Give the plot a main title
+ax.set_title('Box plot of systolic blood pressure (mmHg)')  # Give the plot a main title
 ax.set_ylim(0.0, 300)
 plt.show()
 
-
-fig = plt.figure(figsize=(10, 10)) # Define plot area
-ax = fig.gca() # Define axis
+fig = plt.figure(figsize=(10, 10))  # Define plot area
+ax = fig.gca()  # Define axis
 plt.boxplot(data.loc[:, 'ap_lo'])
-ax.set_title('Box plot of diastolic blood pressure') # Give the plot a main title
+ax.set_title('Box plot of diastolic blood pressure')  # Give the plot a main title
 ax.set_ylim(0.0, 300)
-plt.show()
+plt.show()"""
 
+"""
+Counting Outliers
+"""
 
+# To Numpy
 data_array = data.to_numpy()
 
-#Height Checker
+# Height Checker
 height = data_array[:, 3]
 maxH = 180
 minH = 140
-maxOutlier = height[height>maxH]
-nOutMax = len(maxOutlier)
-print("Count > ", maxH, " is: ", nOutMax)
+maxOutlierH = height[height > maxH]
+nOutMaxH = len(maxOutlierH)
+minOutlierH = height[height < minH]
+nOutMinH = len(minOutlierH)
+totalHeight = nOutMaxH + nOutMinH
 
-minOutlier = height[height<minH]
-nOutMin = len(minOutlier)
-print("Count < ", minH, " is: ", nOutMin)
+print("Height Outliers:")
+print("Count > ", maxH, " is: ", nOutMaxH)
+print("Count < ", minH, " is: ", nOutMinH)
+print("Total Amount of Outliers (height): ", totalHeight)
 
-print("Total Amount of Outliers: ", nOutMax + nOutMin)
+# Weight Checker
+weight = data_array[:, 4]
+maxW = 110
+minW = 40
+maxOutlierW = weight[weight > maxW]
+nOutMaxW = len(maxOutlierW)
+minOutlierW = weight[weight < minW]
+nOutMinW = len(minOutlierW)
+totalWeight = nOutMaxW + nOutMinW
 
-#Weight Checker
-height = data_array[:, 4]
-maxH = 180
-minH = 140
-maxOutlier = height[height>maxH]
-nOutMax = len(maxOutlier)
-print("Count > ", maxH, " is: ", nOutMax)
+print("Weight Outliers:")
+print("Count > ", maxW, " is: ", nOutMaxW)
+print("Count < ", minW, " is: ", nOutMinW)
+print("Total Amount of Outliers (weight): ", totalWeight)
 
-minOutlier = height[height<minH]
-nOutMin = len(minOutlier)
-print("Count < ", minH, " is: ", nOutMin)
+# Systolic Blood Pressure
+sys = data_array[:, 5]
+maxS = 170
+minS = 90
+maxOutlierS = sys[sys > maxS]
+nOutMaxS = len(maxOutlierS)
+minOutlierS = sys[sys < minS]
+nOutMinS = len(minOutlierS)
+totalSys = nOutMaxS + nOutMinS
 
-print("Total Amount of Outliers: ", nOutMax + nOutMin)
+print("Systolic Outliers:")
+print("Count > ", maxS, " is: ", nOutMaxS)
+print("Count < ", minS, " is: ", nOutMinS)
+print("Total Amount of Outliers (Systolic): ", totalSys)
 
-#print(data_array)
+# Diastolic Blood Pressure
+dia = data_array[:, 6]
+maxD = 110
+minD = 70
+maxOutlierD = dia[dia > maxD]
+nOutMaxD = len(maxOutlierD)
+minOutlierD = dia[dia < minD]
+nOutMinD = len(minOutlierD)
+totalDia = nOutMaxD + nOutMinD
+print("Diastolic Outliers:")
+print("Count > ", maxD, " is: ", nOutMaxD)
+print("Count < ", minD, " is: ", nOutMinD)
+print("Total Amount of Outliers (Diastolic): ", totalDia)
+
+total = totalWeight + totalHeight + totalDia + totalSys
+
+print("Total Outliers = ", total)
+print("Good Data = ", 70000 - total)
+
+heightGood = height[height <= maxH]
+heightGood = heightGood[heightGood >= minH]
+
+WGood = weight[weight <= maxW]
+WGood = WGood[WGood >= minW]
+
+SysGood = sys[sys <= maxS]
+SysGood = SysGood[SysGood >= minS]
+
+diaGood = dia[dia <= maxD]
+diaGood = diaGood[diaGood >= minD]
+
+full_data = np.copy(data_array)
+# TO DO: Change gender, make male (2) into 0 value, for chlosterol/gluc change 1->0 2+3->1
+
+# TO DO: We have the clean data in each col, but what we need to do is find the index of the outliers, we need to remove
+#        the outliers row index, which would get rid of the entire row, rather than just the height, weight etc data.
+
+# Pseudo: Find index, take initial data_array, use index to get rid of row in data_array, then take the final array
+#         and make CSV from it, call this cardio_train_clean.csv
+
+# TO DO: analyze newly cleaned data (cardio_train_clean)
+
