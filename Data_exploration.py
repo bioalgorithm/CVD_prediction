2e2089c7b02c1ef05892d3ec67691fd633ae8473
acako <cakoalbina@gmail.com>
2020-10-30 17:49:42 -0400

    data

--- Data_exploration.py
+++ Data_exploration.py
@@ -6,11 +6,16 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 data = pd.read_csv("cardio_train_updated.csv")
-
 print(data)
 
+print(data.dtypes)
 
+data.describe()
 
+data['counts'] = 1
+print(data[['counts', 'gluc']].groupby(['gluc']).agg('count'))
+data['counts'] = 1
+print(data[['counts', '']].groupby(['gluc']).agg('count'))
 
 
 
