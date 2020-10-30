aa88fbeb61b50516ee194d736f1b61510ee54325
acako <cakoalbina@gmail.com>
2020-10-30 18:40:17 -0400

    plots

--- Data_exploration.py
+++ Data_exploration.py
@@ -4,22 +4,48 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-
+pd.set_option('display.max_columns', None)
 data = pd.read_csv("cardio_train_updated.csv")
+'''
 print(data)
 
 print(data.dtypes)
 
-data.describe()
+print(data.describe())
 
 data['counts'] = 1
 print(data[['counts', 'gluc']].groupby(['gluc']).agg('count'))
 data['counts'] = 1
-print(data[['counts', '']].groupby(['gluc']).agg('count'))
+print(data[['counts', 'weight']].groupby(['weight']).agg('count'))
+data['counts'] = 1
+print(data[['counts', 'height']].groupby(['height']).agg('count'))
+data['counts'] = 1
+print(data[['counts', 'smoke']].groupby(['smoke']).agg('count'))
+data['counts'] = 1
+print(data[['counts', 'alco']].groupby(['alco']).agg('count'))
+data['counts'] = 1
+print(data[['counts', 'active']].groupby(['active']).agg('count'))
+data['counts'] = 1
+print(data[['counts', 'cardio']].groupby(['cardio']).agg('count'))
+data['counts'] = 1
+print(data[['counts', 'age']].groupby(['age']).agg('count'))
+data['counts'] = 1
+print(data[['counts', 'ap_hi']].groupby(['ap_hi']).agg('count'))
+data['counts'] = 1
+print(data[['counts', 'cholesterol']].groupby(['cholesterol']).agg('count'))
+'''
+fig = plt.figure(figsize=(10, 10)) # Define plot area
+ax = fig.gca() # Define axis
+plt.boxplot(data.loc[:, 'height'])
+ax.set_title('Box plot of high blood pressure') # Give the plot a main title
+ax.set_ylim(0.0, 300)
+plt.show()
 
-
-
-
-
-
-
+'''
+fig = plt.figure(figsize=(10, 10)) # Define plot area
+ax = fig.gca() # Define axis
+plt.boxplot(data.loc[:, 'ap_lo'])
+ax.set_title('Box plot of high blood pressure') # Give the plot a main title
+ax.set_ylim(0.0, 300)
+plt.show()
+'''
