3cd8b7b7ce802f48c75599b9a5406ff8a135c824
JeffR <jeffrezazada@gmail.com>
2020-11-06 10:35:36 -0500

    Correlation Matrix (REMOVE DIASTOLIC FROM CLEAN, CREATE NEW CSV WITHOUT DIASTOLIC)

--- clean_data_analysis.py
+++ clean_data_analysis.py
@@ -9,32 +9,40 @@ data = pd.read_csv("cardio_train_clean.csv")
 print(data.describe())
 
 data_array = data.to_numpy()
+string = ["does not have CVD", "has CVD"]
 n,m = data_array.shape
-
-index = np.nonzero(data_array[:, -1] == 1)
-k = len(index[0])
-#print(index)
 str = ['gender', 'chlosterol', 'glucose', 'smoke', 'alcohol', 'active']
 id = [2, 7, 8, 9, 10, 11]
 d = len(str)
-bin_array = np.zeros(d)
-for i in range(d):
-    bin = 0
-    for j in range(k):
-        if data_array[index[0][j]][id[i]] == 1:
-            bin += 1
-    print("bin", bin)
-    bin_array[i] = bin / k
 
-print(bin_array)
-bin_array2 = np.ones(d)
-bin_array2 = bin_array2 - bin_array
-print(bin_array2)
-df = pd.DataFrame({'0': bin_array2, '1': bin_array}, index=str)
-ax = df.plot.bar(title = 'If patient has CVD', rot=0)
+for cvd in range(2):
+    index = np.nonzero(data_array[:, -1] == cvd)
+    k = len(index[0])
+    #print(index)
+
+    bin_array = np.zeros(d)
+
+    for i in range(d):
+        bin = 0
+        for j in range(k):
+            if data_array[index[0][j], id[i]] == 1:
+                bin += 1
+        bin_array[i] = bin / k
+
+    bin_array2 = np.ones(d)
+    bin_array2 -= bin_array
+    df = pd.DataFrame({'0': bin_array2, '1': bin_array}, index=str)
+    ax = df.plot.bar(title = 'If patient ' + string[cvd], rot=0)
+    plt.ylim(0, 1)
+    plt.show()
+
+corr_matrix = data.corr()
+sns.heatmap(corr_matrix, annot=True)
 plt.show()
 
-
+corr_matrix = data.corr()
+sns.heatmap(corr_matrix*corr_matrix, annot=True)
+plt.show()
 
 
 '''# histogram for weight
