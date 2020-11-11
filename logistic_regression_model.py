f10d7c558bc85ca604994cee7e5332d0d7b14b9f
acako <cakoalbina@gmail.com>
2020-11-11 11:31:54 -0500

    logistic regression model

---
+++ logistic_regression_model.py
@@ -0,0 +1,64 @@
+import inline as inline
+import matplotlib
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
+import matplotlib.pyplot as plt
+plt.rc("font", size=14)
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+sns.set(style="white")
+sns.set(style="whitegrid", color_codes=True)
+pd.set_option('display.max_columns', None)
+data = pd.read_csv("cardio_train_clean_featureselection.csv")
+print(data.shape)
+print(data.head())
+
+#let's look if data is balanced by looking at target variable
+print(data['Cardio'].value_counts())
+#there are 31181 values of 0 and 30922. Therefore our data is balanced.
+
+sns.countplot(x='Cardio', data=data, palette='hls')
+plt.show()
+plt.savefig('Cariod_data_balance_check')
+#checking percentage of each data variable
+cardio_no = len(data[data['Cardio']==0])
+cardio_yes = len(data[data['Cardio']==1])
+percentage_of_no_CVD = cardio_no/(cardio_no+cardio_yes)
+print("Percentage of people with no CVD is", percentage_of_no_CVD*100)
+percentage_of_CVD = cardio_yes / (cardio_no + cardio_yes)
+print("Percentage of people with CVD is", percentage_of_CVD*100)
+
+#data is balanced so no need to do anymore balancing
+#Percentage of people with no CVD is 50.20852454792845
+#Percentage of people with CVD is 49.79147545207156
+
+#now let us look at the relationship of the CVD to the features
+print(data.groupby('Cardio').mean())
+
+#logistic regression
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+#set x as features and y as target variable
+X = data[['Age', 'Gender','Height', 'Weight', "Systolic BP", "Chlosterol ", "Glucose", "Smoke", "Alcohol", "Active"]]
+y = data['Cardio']
+#train test 75/25 ratio
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.25,random_state=0)
+#load logistic regression
+logistic_regression_model= LogisticRegression(max_iter=100000)
+logistic_regression_model.fit(X_train,y_train)
+y_pred=logistic_regression_model.predict(X_test)
+#confusion matrix
+confusion_matrix = pd.crosstab(y_test, y_pred, rownames=['Actual'], colnames=['Predicted'])
+sns.heatmap(confusion_matrix, annot=True)
+print('Accuracy: ',metrics.accuracy_score(y_test, y_pred))
+plt.show()
+print (X_test)
+print (y_pred)
+
+
+
+
+
