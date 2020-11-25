ea1dcb804f248de5285e1633622e3f4eaace8b24
Jeff <jeffrezazada@gmail.com>
2020-11-24 19:00:09 -0500

    RF tuned

--- random_forest.py
+++ random_forest.py
@@ -16,9 +16,10 @@ from sklearn.ensemble import RandomForestClassifier
 data = pd.read_csv("cardio_train_clean_featureselection.csv")
 
 # Features = xfeat, Target Variable = Y
-X = data[['Age', 'Gender','Height', 'Weight', "Systolic BP", "Chlosterol ", "Glucose", "Smoke", "Alcohol", "Active"]]
+xfeat = data[['Age', 'Gender','Height', 'Weight', "Systolic BP", "Chlosterol ", "Glucose", "Smoke", "Alcohol", "Active"]]
 y = data['Cardio']
 
+x_train, x_test, y_train, y_test = train_test_split(xfeat, y, test_size=0.2, random_state=0)
 from sklearn.model_selection import RandomizedSearchCV
 # Number of trees in random forest
 n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
@@ -43,9 +44,14 @@ random_grid = {'n_estimators': n_estimators,
 print(random_grid)
 # Use the random grid to search for best hyperparameters
 # First create the base model to tune
+
 rf = RandomForestClassifier()
+
 # Random search of parameters, using 3 fold cross validation,
 # search across 100 different combinations, and use all available cores
+
+
+'''
 rf_random = RandomizedSearchCV(estimator = rf, param_distributions = random_grid, n_iter = 100, cv = 3, verbose=2, random_state=42, n_jobs = -1)
 # Fit the random search model
 rf_random.fit(X, y)
@@ -53,3 +59,26 @@ rf_random.fit(X, y)
 best_random = rf_random.best_estimator_
 
 print('Config: ', rf_random.best_params_)
+'''
+
+rf.fit(x_train, y_train)
+y_pred_rf = rf.predict(x_test)
+
+print()
+print('INITIAL rf Metrics:')
+print('Accuracy: ', metrics.accuracy_score(y_test, y_pred_rf))
+print('Precision: ', metrics.precision_score(y_test, y_pred_rf))
+print('Recall: ', metrics.recall_score(y_test, y_pred_rf))
+print()
+
+rf = RandomForestClassifier(n_estimators= 200, min_samples_split = 5, min_samples_leaf = 4, max_features = 'auto',
+                            max_depth= 10, bootstrap = True)
+rf.fit(x_train, y_train)
+y_pred_rf = rf.predict(x_test)
+
+print()
+print('Tuned rf Metrics:')
+print('Accuracy: ', metrics.accuracy_score(y_test, y_pred_rf))
+print('Precision: ', metrics.precision_score(y_test, y_pred_rf))
+print('Recall: ', metrics.recall_score(y_test, y_pred_rf))
+print()
