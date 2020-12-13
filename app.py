d577529c2960b06f1cb72895af6a36e35b819d8f
Jeff <jeffrezazada@gmail.com>
2020-12-12 21:32:54 -0500

    update of app

--- app.py
+++ app.py
@@ -8,10 +8,9 @@ from dash.dependencies import Input, Output, State
 
 app = dash.Dash(external_stylesheets=[dbc.themes.DARKLY])
 app.title = 'Cardiovascular Disease Predictor'
-# ------------------------------------------------------------------------------
-# Import and clean data (importing csv into pandas)
-df = pd.read_csv("cardio_train_clean_1hot_featureselection.csv")
-model = joblib.load('logistic_regression_model_1hot.sav')
+
+df = pd.read_csv("cardio_train_clean_featureselection.csv")
+model = joblib.load('logistic_regression_model.sav')
 
 layout = dict(
     autosize=True,
@@ -59,20 +58,20 @@ app.layout = html.Div([
                         ),
                     ],
                     className='one columns',
-                    style={'margin-left': 10, 'padding': 10}
+                    style={'padding': 10}
                 )),
 
                 dbc.Col(html.Div(
                     [
-                        html.P('Age (Years): '),
+                        html.P('Age (years): '),
                         dbc.Input(
                             id="age", type="number",
-                            debounce=True, placeholder="30 - 80 years", min=10, max=99
+                            debounce=True, placeholder="30 - 80 years", min=30, max=80
 
                         ),
                     ],
                     className='three columns',
-                    style={'margin-left': 10, 'padding': 10}
+                    style={'padding': 10}
                 )),
 
                 dbc.Col(html.Div(
@@ -85,7 +84,7 @@ app.layout = html.Div([
                         ),
                     ],
                     className='three columns',
-                    style={'margin-left': 10, 'padding': 10}
+                    style={'padding': 10}
                 )),
 
                 dbc.Col(html.Div(
@@ -98,12 +97,12 @@ app.layout = html.Div([
                         ),
                     ],
                     className='three columns',
-                    style={'margin-left': 10, 'padding': 10}
+                    style={'padding': 10}
                 )),
 
                 dbc.Col(html.Div(
                     [
-                        html.P('Systolic Blood Pressure (mmHg): '),
+                        html.P('Systolic BP (mmHg): '),
                         dbc.Input(
                             id="sys", type="number",
                             debounce=True, placeholder="90 - 170 mmHg", min=90, max=170
@@ -191,8 +190,7 @@ app.layout = html.Div([
             className='row2'
         ),
 
-        dbc.Button('Submit', size="lg", color="light", id='submit-val', n_clicks=0, style={'padding': 10}),
-        dbc.Button('Reset', size="sm", color="light", id='reset', n_clicks=0, style={'margin-left': 20, 'padding': 10}),
+        dbc.Button('Submit', size="lg", color="light", id='submit-val', n_clicks=0, style={'margin-top': 30,'padding': 10})
 
     ], className='row0', style={'margin-top': 10, 'border':
                                 '1px solid #C6CCD5', 'padding': 10,
@@ -201,8 +199,8 @@ app.layout = html.Div([
     html.Br(),
     dbc.Row(html.Div(
         id='result',
-        style={'margin-top': 10, 'border': '1px solid #C6CCD5',
-               'padding': 10, 'border-radius': '5px'}))
+        style={'margin-top': 10,
+               'padding': 10}))
 
 ], style={'padding': '25px'})
 
@@ -231,21 +229,20 @@ def prediction(s_clicks, gender, age, height, weight, sys, chol, gluc, smoke, al
         clicked = True
 
     # Error Catch
-    if not age:
+    if not age or not isinstance(age, (int, float, complex)) and not isinstance(age, bool):
         age = 0
-    if not height:
+    if not height or not isinstance(height, (int, float, complex)) and not isinstance(height, bool):
         height = 0
-    if not weight:
+    if not weight or not isinstance(weight, (int, float, complex)) and not isinstance(weight, bool):
         weight = 0
-    if not sys:
+    if not sys or not isinstance(sys, (int, float, complex)) and not isinstance(sys, bool):
         sys = 0
-
 
     # Input into dataset
     if clicked:
-        data = np.array([
+        data = np.array([[
             age, gender, height, weight, sys,
-            chol, gluc, smoke, alc, active
+            chol, gluc, smoke, alc, active],
         ])
         data.reshape(1, -1)
 
@@ -254,36 +251,25 @@ def prediction(s_clicks, gender, age, height, weight, sys, chol, gluc, smoke, al
         return "The prediction for the probability of CVD is: ", round(y_prob[0, 1] * 100, 3), '%'
 
 
-@app.callback([Output('gender', 'value'),
-               Output('age', 'value'),
-               Output('height', 'value'),
-               Output('weight', 'value'),
-               Output('sys', 'value'),
-               Output('chol', 'value'),
-               Output('gluc', 'value'),
-               Output('smoke', 'value'),
-               Output('alc', 'value'),
-               Output('active', 'value')],
+@app.callback([
+    Output('gender', 'value'),
+    Output('age', 'value'),
+    Output('height', 'value'),
+    Output('weight', 'value'),
+    Output('sys', 'value'),
+    Output('chol', 'value'),
+    Output('gluc', 'value'),
+    Output('smoke', 'value'),
+    Output('alc', 'value'),
+    Output('active', 'value')],
 
-              [Input('reset', 'n_clicks')])
+    [Input('submit-val', 'n_clicks')])
 def update(reset):
-    print(reset)
-    if reset != 0:
-        gender = 0
-        age = "30 - 80 years"
-        height = 0
-        weight = 0
-        sys = 0
-        chol = 0
-        gluc = 0
-        smoke = 0
-        alc = 0
-        active = 0
-        return gender, age, height, weight, sys, chol, gluc, smoke, alc, active
-
+        return 0, "30 - 80 years", "140 - 180 cm", "40 - 110 kg", "90 - 170 mmHg", 0, 0, 0, 0, 0
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
 '''
     html.Div(
             [
